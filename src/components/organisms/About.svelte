e8ccfdf9ec31a0f3b3a433493474849c38f9e38d
vornex <ajay.ramnath8812@gmail.com>
2024-02-06 01:00:39 -0400

    fullstop no worky

--- src/components/organisms/About.svelte
+++ src/components/organisms/About.svelte
@@ -41,7 +41,7 @@
 			, aside from this I also upload random .mp4 files now and then
 			<Tooltip tip="🎵">
 				<a href="https://cdn-vornexx.vercel.app" target="_blank" rel="noreferrer">
-					<span>here</span>.
+					<span>here</span> .
 				</a>
 			</Tooltip>
 
